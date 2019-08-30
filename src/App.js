6de1e61bbc95afa8b65886d46a478d97cefb457b
cedmail <cmailleux@jahia.com>
2019-08-30 10:59:18 -0400

    BACKLOG-10985 : Fix tests

--- src/App.js
+++ src/App.js
@@ -1,208 +1,204 @@
-import React from "react";
-import moment from "moment";
+import React from 'react';
+import moment from 'moment';
 
 import JahiaSearchAPIConnector from '@jahia/search-ui-jahia-connector/src';
 import {
-  ErrorBoundary,
-  Facet,
-  SearchProvider,
-  SearchBox,
-  Results,
-  PagingInfo,
-  ResultsPerPage,
-  Paging,
-  Sorting,
-  WithSearch
-} from "@elastic/react-search-ui";
-import {
-  Layout,
-  SingleSelectFacet,
-  SingleLinksFacet
-} from "@elastic/react-search-ui-views";
-import "@elastic/react-search-ui-views/lib/styles/styles.css";
+    ErrorBoundary,
+    Paging,
+    PagingInfo,
+    Results,
+    ResultsPerPage,
+    SearchBox,
+    SearchProvider,
+    WithSearch
+} from '@elastic/react-search-ui';
+import {Layout} from '@elastic/react-search-ui-views';
+import '@elastic/react-search-ui-views/lib/styles/styles.css';
 
 const SORT_OPTIONS = [
-  {
-    name: "Relevance",
-    value: "",
-    direction: ""
-  },
-  {
-    name: "Title",
-    value: "title",
-    direction: "asc"
-  }
+    {
+        name: 'Relevance',
+        value: '',
+        direction: ''
+    },
+    {
+        name: 'Title',
+        value: 'title',
+        direction: 'asc'
+    }
 ];
 
 let connector = new JahiaSearchAPIConnector({
-    apiToken:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwOi8vamFoaWEuY29tIiwic3ViIjoiYXBpIHZlcmlmaWNhdGlvbiIsImlzcyI6ImR4Iiwic2NvcGVzIjpbIm15YXBwIl0sImlhdCI6MTU2NzAxMTY2NiwianRpIjoiYTFmMDkwMzYtMTBkMy00NWI3LTkyNzgtZDlkMGIwZmQ0OTdiIn0.m4Xqog-zOpzCfP1w658l5l6qejtH-vgJCGEoK7ap4dI',
-    baseURL:'http://localhost:8080'
-  });
-
+    apiToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwOi8vamFoaWEuY29tIiwic3ViIjoiYXBpIHZlcmlmaWNhdGlvbiIsImlzcyI6ImR4Iiwic2NvcGVzIjpbIm15YXBwIl0sImlhdCI6MTU2NzAxMTY2NiwianRpIjoiYTFmMDkwMzYtMTBkMy00NWI3LTkyNzgtZDlkMGIwZmQ0OTdiIn0.m4Xqog-zOpzCfP1w658l5l6qejtH-vgJCGEoK7ap4dI',
+    baseURL: 'http://localhost:8080',
+    siteKey: 'academy',
+    language: 'en',
+    workspace: 'LIVE'
+});
 
 const config = {
-  searchQuery: {
-    result_fields: {
-      visitors: { raw: {} },
-      world_heritage_site: { raw: {} },
-      location: { raw: {} },
-      acres: { raw: {} },
-      square_km: { raw: {} },
-      title: {
-        snippet: {
-          size: 100,
-          fallback: true
-        }
-      },
-      link: { raw: {} },
-      states: { raw: {} },
-      date_established: { raw: {} },
-      description: {
-        snippet: {
-          size: 100,
-          fallback: true
-        }
-      }
-    },
-    disjunctiveFacets: ["acres", "states", "date_established", "location"],
-    facets: {
-      world_heritage_site: { type: "value" },
-      states: { type: "value", size: 30 },
-      acres: {
-        type: "range",
-        ranges: [
-          { from: -1, name: "Any" },
-          { from: 0, to: 1000, name: "Small" },
-          { from: 1001, to: 100000, name: "Medium" },
-          { from: 100001, name: "Large" }
-        ]
-      },
-      location: {
-        // San Francisco. In the future, make this the user's current position
-        center: "37.7749, -122.4194",
-        type: "range",
-        unit: "mi",
-        ranges: [
-          { from: 0, to: 100, name: "Nearby" },
-          { from: 100, to: 500, name: "A longer drive" },
-          { from: 500, name: "Perhaps fly?" }
-        ]
-      },
-      date_established: {
-        type: "range",
-
-        ranges: [
-          {
-            from: moment()
-              .subtract(50, "years")
-              .toISOString(),
-            name: "Within the last 50 years"
-          },
-          {
-            from: moment()
-              .subtract(100, "years")
-              .toISOString(),
-            to: moment()
-              .subtract(50, "years")
-              .toISOString(),
-            name: "50 - 100 years ago"
-          },
-          {
-            to: moment()
-              .subtract(100, "years")
-              .toISOString(),
-            name: "More than 100 years ago"
-          }
-        ]
-      },
-      visitors: {
-        type: "range",
-        ranges: [
-          { from: 0, to: 10000, name: "0 - 10000" },
-          { from: 10001, to: 100000, name: "10001 - 100000" },
-          { from: 100001, to: 500000, name: "100001 - 500000" },
-          { from: 500001, to: 1000000, name: "500001 - 1000000" },
-          { from: 1000001, to: 5000000, name: "1000001 - 5000000" },
-          { from: 5000001, to: 10000000, name: "5000001 - 10000000" },
-          { from: 10000001, name: "10000001+" }
-        ]
-      }
-    }
-  },
-  autocompleteQuery: {
-    results: {
-      resultsPerPage: 10,
-      result_fields: {
-        title: {
-          snippet: {
-            size: 100,
-            fallback: true
-          }
+    searchQuery: {
+        result_fields: {
+            visitors: {raw: {}},
+            world_heritage_site: {raw: {}},
+            location: {raw: {}},
+            acres: {raw: {}},
+            square_km: {raw: {}},
+            title: {
+                snippet: {
+                    size: 100,
+                    fallback: true
+                }
+            },
+            link: {raw: {}},
+            states: {raw: {}},
+            date_established: {raw: {}},
+            description: {
+                snippet: {
+                    size: 100,
+                    fallback: true
+                }
+            }
         },
-        link: {
-          raw: {}
+        disjunctiveFacets: ['acres', 'states', 'date_established', 'location'],
+        facets: {
+            world_heritage_site: {type: 'value'},
+            states: {type: 'value', size: 30},
+            acres: {
+                type: 'range',
+                ranges: [
+                    {from: -1, name: 'Any'},
+                    {from: 0, to: 1000, name: 'Small'},
+                    {from: 1001, to: 100000, name: 'Medium'},
+                    {from: 100001, name: 'Large'}
+                ]
+            },
+            location: {
+                // San Francisco. In the future, make this the user's current position
+                center: '37.7749, -122.4194',
+                type: 'range',
+                unit: 'mi',
+                ranges: [
+                    {from: 0, to: 100, name: 'Nearby'},
+                    {from: 100, to: 500, name: 'A longer drive'},
+                    {from: 500, name: 'Perhaps fly?'}
+                ]
+            },
+            date_established: {
+                type: 'range',
+
+                ranges: [
+                    {
+                        from: moment()
+                            .subtract(50, 'years')
+                            .toISOString(),
+                        name: 'Within the last 50 years'
+                    },
+                    {
+                        from: moment()
+                            .subtract(100, 'years')
+                            .toISOString(),
+                        to: moment()
+                            .subtract(50, 'years')
+                            .toISOString(),
+                        name: '50 - 100 years ago'
+                    },
+                    {
+                        to: moment()
+                            .subtract(100, 'years')
+                            .toISOString(),
+                        name: 'More than 100 years ago'
+                    }
+                ]
+            },
+            visitors: {
+                type: 'range',
+                ranges: [
+                    {from: 0, to: 10000, name: '0 - 10000'},
+                    {from: 10001, to: 100000, name: '10001 - 100000'},
+                    {from: 100001, to: 500000, name: '100001 - 500000'},
+                    {from: 500001, to: 1000000, name: '500001 - 1000000'},
+                    {from: 1000001, to: 5000000, name: '1000001 - 5000000'},
+                    {from: 5000001, to: 10000000, name: '5000001 - 10000000'},
+                    {from: 10000001, name: '10000001+'}
+                ]
+            }
         }
-      }
     },
-    suggestions: {
-      types: {
-        documents: {
-          fields: ["title"]
+    autocompleteQuery: {
+        results: {
+            resultsPerPage: 10,
+            result_fields: {
+                title: {
+                    snippet: {
+                        size: 100,
+                        fallback: true
+                    }
+                },
+                link: {
+                    raw: {}
+                }
+            }
+        },
+        suggestions: {
+            types: {
+                documents: {
+                    fields: ['title']
+                }
+            },
+            size: 4
         }
-      },
-      size: 4
-    }
-  },
-  apiConnector: connector,
-  hasA11yNotifications: true
+    },
+    apiConnector: connector,
+    hasA11yNotifications: true
 };
 
 export default function App() {
-  return (
-    <SearchProvider config={config}>
-      <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
-        {({ wasSearched }) => {
-          return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={
-                    <SearchBox
-                      autocompleteMinimumCharacters={1}
-                      autocompleteResults={{
-                        linkTarget: "_blank",
-                        sectionTitle: "Results",
-                        titleField: "title",
-                        urlField: "link",
-                        shouldTrackClickThrough: true,
-                        clickThroughTags: ["test"]
-                      }}
-                      autocompleteSuggestions={false}
-                      debounceLength={0}
-                      searchAsYouType={false}
-                      useAutocomplete={true}
-                    />
-                  }
-                  bodyContent={
-                    <Results
-                      titleField="title"
-                      urlField="link"
-                      shouldTrackClickThrough={true}
-                    />
-                  }
-                  bodyHeader={
-                    <React.Fragment>
-                      {wasSearched && <PagingInfo />}
-                      {wasSearched && <ResultsPerPage />}
-                    </React.Fragment>
-                  }
-                  bodyFooter={<Paging />}
-                />
-              </ErrorBoundary>
-            </div>
-          );
-        }}
-      </WithSearch>
-    </SearchProvider>
-  );
+    return (
+        <SearchProvider config={config}>
+            <WithSearch mapContextToProps={({wasSearched}) => ({wasSearched})}>
+                {({wasSearched}) => {
+                    return (
+                        <div className="App">
+                            <ErrorBoundary>
+                                <Layout
+                                    header={
+                                        <SearchBox
+                                            autocompleteMinimumCharacters={1}
+                                            autocompleteResults={{
+                                                linkTarget: '_blank',
+                                                sectionTitle: 'Results',
+                                                titleField: 'title',
+                                                urlField: 'link',
+                                                shouldTrackClickThrough: true,
+                                                clickThroughTags: ['test']
+                                            }}
+                                            autocompleteSuggestions={false}
+                                            debounceLength={0}
+                                            searchAsYouType={false}
+                                            useAutocomplete={true}
+                                        />
+                                    }
+                                    bodyContent={
+                                        <Results
+                                            titleField="title"
+                                            urlField="link"
+                                            shouldTrackClickThrough={true}
+                                        />
+                                    }
+                                    bodyHeader={
+                                        <React.Fragment>
+                                            {wasSearched && <PagingInfo/>}
+                                            {wasSearched && <ResultsPerPage/>}
+                                        </React.Fragment>
+                                    }
+                                    bodyFooter={<Paging/>}
+                                />
+                            </ErrorBoundary>
+                        </div>
+                    );
+                }}
+            </WithSearch>
+        </SearchProvider>
+    );
 }
